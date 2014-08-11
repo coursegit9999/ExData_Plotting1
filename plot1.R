652ba13eab5e8e03a3096efd2e26c53ab3ad9625
coursegit9999 <lazorik+courseragit@gmail.com>
2014-08-10 21:05:52 -0400

    assignment submitted

--- plot1.R
+++ plot1.R
@@ -1,1 +1,19 @@
+plot1 <- function() {
+
+setwd("D:/R/RClass/EDA_Proj1")
+    
 tabrows <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
+
+tabrows$Date <- as.Date(tabrows$Date , "%d/%m/%Y")
+
+tabsel <-  tabrows[ tabrows$Date >= as.Date('2007-02-01') & tabrows$Date <= as.Date('2007-02-02') & tabrows$Global_active_power != "?",]
+
+tabsel$Global_active_power <- as.numeric(tabsel$Global_active_power)
+
+png('plot1.png', width = 480, height = 480, units = "px")
+
+hist(tabsel$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)", ylim=c(0,1200))
+
+dev.off()
+
+}
